8ceeaae4111a9c07f4348f07eaaceb90a0495385
Dancalif <dancalif@gmail.com>
2016-02-18 16:36:36 -0800

    Updating Monster script

--- src/test/java/com/sqa/du/monster/MonsterTest.java
+++ src/test/java/com/sqa/du/monster/MonsterTest.java
@@ -2,7 +2,6 @@ package com.sqa.du.monster;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -15,13 +14,13 @@ public class MonsterTest {
 	public static String[] keywords = { "Java", "Automation", "Selenium" };
 	public static String userName = "dancalif@gmail.com";
 	public static String userPassword = "testtest123";
-	public static WebElement element;
+	public static String inputCity = "San Francisco";
 	private WebDriver driver;
 
 	@AfterClass
 	public void afterClass() {
 		// Quit Driver
-		// driver.quit();
+		driver.quit();
 	}
 
 	@BeforeClass
@@ -41,18 +40,20 @@ public class MonsterTest {
 	@Test(dataProvider = "test1")
 	public void test1(String testName, String[] keywords, int maxResultsNum, int requiredResultsNum)
 			throws InterruptedException {
-		Thread.sleep(2000);
-		element = driver.findElement(By.id("loginLink"));
-		if (element.isDisplayed()) {
-			element.click();
-		} else {
-			System.out.println("Couldn't find your f...ing element!!!");
+		// Thread.sleep(2000);
+		driver.findElement(By.linkText("Log In")).click();
+		driver.findElement(By.id("EmailAddress")).sendKeys(userName);
+		driver.findElement(By.id("Password")).sendKeys(userPassword);
+		driver.findElement(By.xpath(".//*[@id='signInContent']/form/div[3]/input[1]")).click();
+
+		if (driver.findElement(By.xpath("//*[@id='header']/div/div[2]/a")).isDisplayed()) {
+			driver.findElement(By.xpath("//*[@id='header']/div/div[2]/a")).click();
 		}
 
-		// By.id("loginLink")).isDisplayed());
-		// driver.findElement(By.id("EmailAddress")).sendKeys(userName);
-		// driver.findElement(By.id("Password")).sendKeys(userPassword);
-		// driver.findElement(By.xpath(".//*[@id='signInContent']/form/div[3]/input[1]")).click();
+		driver.findElement(By.id("ctl00__powerSearchControl__ptbJobTitle"))
+				.sendKeys(keywords[0] + ", " + keywords[1] + ", " + keywords[2]);
+		driver.findElement(By.id("ctl00__powerSearchControl__ptbLocations")).sendKeys(inputCity);
+		driver.findElement(By.id("ctl00__powerSearchControl__btnSearch")).click();
 	}
 
 }
